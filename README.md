b5ef34e4c79218684ea4f3f181c1435a4b7a0d11
Rashid Khan <github.fliplap@spamgourmet.com>
2018-10-19 14:59:53 -0700

    First push

---
+++ README.md
@@ -0,0 +1,1 @@
+This is my personal node app seed. Don't use it. Or do. Whatever makes you feel better.
---
+++ main.js
@@ -0,0 +1,61 @@
+import { get } from 'axios';
+import elasticsearch from 'elasticsearch';
+import { sortBy } from 'lodash';
+
+const es = new elasticsearch.Client({
+  host: 'localhost:9200',
+});
+
+function fetchFlightData() {
+  return get(
+    'http://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=33.433638&lng=-112.008113&fDstL=0&fDstU=100'
+  ).then(resp => resp.data.acList);
+}
+
+function indexFlightData() {
+  return fetchFlightData()
+    .then(list => sortBy(list, 'Lat'))
+    .then(list => {
+      list.forEach(ac => {
+        ac['@timestamp'] = new Date(ac.PosTime).getTime();
+        ac.location = {
+          lat: ac.Lat,
+          lon: ac.Long,
+        };
+
+        console.log(ac);
+
+        es.index({
+          index: 'adsb',
+          type: 'ac',
+          id: `${ac.Id}-${ac.PosTime}`,
+          body: ac,
+        }).then(console.log);
+      });
+      return true;
+    });
+}
+
+function go() {
+  indexFlightData().then(() => setTimeout(go, 5000));
+}
+
+es.indices
+  .create({
+    index: 'adsb',
+    body: {
+      settings: {
+        number_of_shards: 1,
+      },
+      mappings: {
+        ac: {
+          properties: {
+            location: { type: 'geo_point' },
+            '@timestamp': { type: 'date' },
+          },
+        },
+      },
+    },
+  })
+  .then(go)
+  .catch(go);
